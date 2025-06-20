2bff201f340b86c9c0467e64b46349571f276e6f
Rishabh <40533251+Rishabh5321@users.noreply.github.com>
2025-06-20 10:30:30 +0530

    Add rust-overlay and refactor zed package variants
    The commit adds rust-overlay for building from source and reorganizes
    zed-editor package variants to support both binary and FHS builds.

--- flake.nix
+++ flake.nix
@@ -1,23 +1,50 @@
 {
-  description = "Flake with package defined in package.nix";
+  description = "Simple zed-editor flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, rust-overlay }:
     let
       forAllSystems = f: nixpkgs.lib.genAttrs [ "x86_64-linux" ] (system:
         f {
           inherit system;
-          pkgs = import nixpkgs { inherit system; };
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay.overlays.default ];
+          };
         }
       );
     in
     {
-      packages = forAllSystems ({ pkgs, ... }: {
-        zed-editor = pkgs.callPackage ./packages/zed-editor { };
-        zed-editor-preview = pkgs.callPackage ./packages/zed-editor-preview { };
-        default = pkgs.callPackage ./packages/zed-editor { };
-      });
+      packages = forAllSystems ({ pkgs, ... }:
+        let
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = pkgs.rust-bin.stable.latest.default;
+            rustc = pkgs.rust-bin.stable.latest.default;
+          };
+        in
+        {
+          zed-editor = pkgs.callPackage ./packages/zed-editor {
+            inherit rustPlatform;
+          };
+          zed-editor-bin = pkgs.callPackage ./packages/zed-editor-bin { };
+          zed-editor-fhs = self.packages.${pkgs.system}.zed-editor.fhs;
+          zed-editor-bin-fhs = self.packages.${pkgs.system}.zed-editor-bin.fhs;
+          zed-editor-preview = pkgs.callPackage ./packages/zed-editor-preview {
+            inherit rustPlatform;
+          };
+          zed-editor-preview-bin = pkgs.callPackage ./packages/zed-editor-preview-bin { };
+          zed-editor-preview-fhs = self.packages.${pkgs.system}.zed-editor-preview.fhs;
+          zed-editor-preview-bin-fhs = self.packages.${pkgs.system}.zed-editor-preview-bin.fhs;
+          default = pkgs.callPackage ./packages/zed-editor { };
+        }
+      );
 
       # Optional: uncomment to enable devShell
       # devShells = forAllSystems ({ pkgs, ... }: {
